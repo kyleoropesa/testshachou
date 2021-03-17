56361d3cd732ef990343d9a6d65d5706f7be9ea5
carlo-oropesa <carlo.oropesa@quipper.com>
2021-03-17 18:28:26 +0800

    added implementation details for endpoints

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from typing import List, Dict
+from uuid import uuid4
+from models.projects import *
 
 app = FastAPI()
 
+projects_db: Dict = {}
+
+
 @app.get("/projects")
 async def get_all_projects():
-    pass
+    return list(projects_db.values())
 
-@app.post("/projects/create")
-async def create_project():
-    pass
+
+@app.post("/projects/create", status_code=status.HTTP_201_CREATED)
+async def create_project(request: ProjectRequestModel):
+    project = ProjectResponseModel(
+        id=uuid4(),
+        title=request.title,
+        description=request.description,
+        owner=request.owner,
+        created_at=datetime.utcnow(),
+        updated_at=datetime.utcnow(),
+        tags=request.tags,
+        active=True
+    )
+    projects_db[str(project.id)] = project
+    return project
 
 
 @app.get("/projects/{project_id}")
-async def get_project_details(project_id):
-    pass
+async def get_project_details(project_id, response: Response):
+    try:
+        return projects_db[project_id]
+    except KeyError:
+        response.status_code = status.HTTP_404_NOT_FOUND
+
 
 @app.delete("/projects/{project_id}")
-async def delete_project(project_id):
-    pass
+async def delete_project(project_id, response: Response):
+    try:
+        project: ProjectResponseModel = projects_db[project_id]
+        project.active = False
+    except KeyError:
+        response.status_code = status.HTTP_404_NOT_FOUND
+
 
 @app.put("/projects/{project_id}/update")
-async def update_project(project_id):
-    pass
+async def update_project(project_id, request: ProjectRequestModel, response: Response):
+    try:
+        project: ProjectResponseModel = projects_db[project_id]
+        project.title = request.title
+        project.description = request.description
+        project.owner = request.owner
+        project.tags = request.tags
+        project.updated_at = datetime.utcnow()
+        return project
+    except KeyError:
+        response.status_code = status.HTTP_404_NOT_FOUND
 
 
 @app.post("/projects/{project_id}/testcase/create")
@@ -42,6 +78,3 @@ async def delete_testcase(project_id, testcase_id):
 @app.put("/projects/{project_id}/testcase/{testcase_id}/update")
 async def update_testcase(project_id, testcase_id):
     pass
-
-
-
