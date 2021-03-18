013fffec66fd8a387a8c0fa51e39ac06c8844e99
carlo-oropesa <carlo.oropesa@quipper.com>
2021-03-18 20:59:59 +0800

    added test scenarios for create project endpoint

---
+++ tests/projects.py
@@ -0,0 +1,13 @@
+class ProjectsTest:
+    def test_successful_project_creation(self):
+        pass
+
+    def test_create_project_with_invalid_format_in_request_body(self):
+        pass
+
+    def test_create_project_with_empty_values_on_optional_fields(self):
+        pass
+
+    def test_create_project_with_non_existing_keys_for_optional_values_in_request_body(self):
+        pass
+
