a5e0ac52a4a68d2556fe20d89987020195c92468
carlo-oropesa <carlo.oropesa@quipper.com>
2021-03-22 19:40:16 +0800

    added field level validations in ProjectRequestModel

--- models/projects.py
+++ models/projects.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import datetime
 from uuid import UUID
 
@@ -9,6 +9,41 @@ class ProjectRequestModel(BaseModel):
     description: Optional[str] = None
     owner: str
     tags: List[str]
+
+    @validator('title', 'owner')
+    def fields_should_not_be_space_only(cls, value: str):
+        if value.isspace():
+            raise ValueError('Spaces are confusing, use alphanumeric characters instead')
+        else:
+            return value
+
+    @validator('description')
+    def description_should_not_be_space_only(cls, value: str):
+        if value is str:
+            if value.isspace():
+                raise ValueError('Spaces are confusing, use alphanumeric characters instead')
+        return value
+
+    @validator('title', 'owner', 'description')
+    def fields_should_not_be_empty_string(cls, value: str):
+        if value == "":
+            raise ValueError('Empty strings are not allowed')
+        else:
+            return value
+
+    @validator('tags')
+    def tag_values_should_not_be_space_only(cls, tag_list: list):
+        for tags in tag_list:
+            if tags.isspace():
+                raise ValueError('Spaces are confusing, use alphanumeric characters instead')
+        return tag_list
+
+    @validator('tags')
+    def tag_values_should_not_be_empty_string(cls, tag_list: list):
+        for tags in tag_list:
+            if tags == "":
+                raise ValueError('Empty strings are not allowed')
+        return tag_list
 
 
 class ProjectResponseModel(ProjectRequestModel):
