cb08c4eaf1c0c82b04847bccc08cb68010242c95
carlo-oropesa <carlo.oropesa@quipper.com>
2021-03-27 10:00:24 +0800

    add TestCaseResponseModel
    Co-authored-by: kyleoropesa <kyleoropesa@gmail.com>

--- models/testcase.py
+++ models/testcase.py
@@ -1,9 +1,19 @@
-from typing import Optional, List
+from typing import List
 from pydantic import BaseModel, validator
+from datetime import datetime
+from uuid import UUID
 
-class TestCaseModel(BaseModel):
+
+class TestCaseRequestModel(BaseModel):
     title: str
     description: str
     author: str
     tags: List[str]
     expected_results: str
+
+
+class TestCaseResponseModel(BaseModel):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime
+    updated_by: str
