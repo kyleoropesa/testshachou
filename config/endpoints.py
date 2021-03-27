bba3da75a8d5dd91b9d1330cbadc8f9e2d711b95
carlo-oropesa <carlo.oropesa@quipper.com>
2021-03-27 10:56:41 +0800

    add url config for testcase endpoints
    Co-authored-by: kyleoropesa <kyleoropesa@gmail.com>

--- config/endpoints.py
+++ config/endpoints.py
@@ -7,3 +7,13 @@ class Project(BaseConfig):
     GET_PROJECT_DETAILS: str = "/projects/{project_id}"
     DELETE_PROJECT: str = "/projects/{project_id}"
     UPDATE_PROJECT: str = "/projects/{project_id}/update"
+
+
+class TestCase(BaseConfig):
+    PROJECT_PATH: str = "/projects/{project_id}"
+
+    GET_ALL_TESTCASE: str = f"{PROJECT_PATH}" + "/testcase"
+    GET_TESTCASE_DETAIL: str = f"{PROJECT_PATH}" + "/testcase/{testcase_id}"
+    CREATE_TESTCASE: str = f"{PROJECT_PATH}" + "/testcase/create"
+    DELETE_TESTCASE: str = f"{PROJECT_PATH}" + "/testcase/{testcase_id}/delete"
+    UPDATE_TESTCASE: str = f"{PROJECT_PATH}" + "/testcase/{testcase_id}/update"
